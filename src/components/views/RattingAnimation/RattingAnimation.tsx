8404b873d5ab78cf3baab2baac0fba80615d4c7c
清水ハイネ <heine.shimizu@gmail.com>
2020-02-04 00:15:52 +0900

    かくつきに対するコメントを追加。クラス名修正

--- src/components/views/RattingAnimation/RattingAnimation.tsx
+++ src/components/views/RattingAnimation/RattingAnimation.tsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import ImageButton from 'components/atoms/ImageButton/ImageButton';
 
-class rattingAnimation extends Component {
+class RattingAnimation extends Component {
   render() {
     return (
       <div>
         <h1>Ratting Animation</h1>
         <h2>IE/Edgeにおいて、animation/transitionを使用して画像を移動・拡大縮小させるとカクカクするバグ</h2>
         <ImageButton />
+         <p>animation/transitionをさせたい要素に
+         <br />
+         <code>transform: scale(1.0, 1.0) rotate(0.0001deg)</code>
+         <br />
+         を足してハードウェアレンダリングを行わせるようにする。</p>
       </div>
     );
   }
 }
 
-export default rattingAnimation;
+export default RattingAnimation;
